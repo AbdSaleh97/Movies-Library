abcbb15f804d6f0892f0b43d57c13f4ffb63e7dd
AbdSaleh97 <salehabd.897@gmail.com>
2024-02-13 21:37:22 +0300

    lab11 mod

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const moviesData = require('./Movie Data/data.json');
-
+//Lab11
 app.get('/', homeHandler);
 
 function homeHandler(req, res) {
